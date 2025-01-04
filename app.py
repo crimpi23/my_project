6d6a15450c97e31b135e9e05e527a58f8b72101e
crimpi23 <crimpi@gmail.com>
2025-01-04 17:59:13 +0200

    Fixed Flask and Werkzeug versions, updated styles and index

--- app.py
+++ app.py
@@ -1,48 +1,11 @@
-<<<<<<< HEAD
-from flask import Flask, request, render_template
-import psycopg2
-
-app = Flask(__name__)
-
-# Конфігурація підключення до бази даних PostgreSQL
-DB_CONFIG = {
-    "host": "dpg-cts5hh5umphs73fk5pvg-a.frankfurt-postgres.render.com",
-    "database": "crimpi_parts",
-    "user": "crimpi_parts_user",
-    "password": "ваш_пароль"
-}
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    price = None
-    if request.method == 'POST':
-        article = request.form.get('article')
-        if article:
-            # Підключення до бази даних
-            conn = psycopg2.connect(**DB_CONFIG)
-            cursor = conn.cursor()
-            cursor.execute("SELECT price FROM public.vag WHERE article = %s", (article,))
-            result = cursor.fetchone()
-            if result:
-                price = result[0]
-            else:
-                price = "Артикул не знайдено"
-            cursor.close()
-            conn.close()
-    return render_template('index.html', price=price)
-
-if __name__ == '__main__':
-    app.run(debug=True)
-=======
 import os
-
-# Додаємо шлях до бібліотеки вручну
-os.add_dll_directory(r"C:\Program Files\PostgreSQL\17\bin")
-
 import psycopg2
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
+
+# Додаємо шлях до бібліотеки вручну
+os.add_dll_directory(r"C:\Program Files\PostgreSQL\17\bin")
 
 # Функція для підключення до бази
 def get_connection():
@@ -80,4 +43,3 @@ def search():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
->>>>>>> 409a251 (Initial commit)

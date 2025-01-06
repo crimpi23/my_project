5c837d0447a9a9172a4e718731881046de1a32d4
crimpi23 <crimpi@gmail.com>
2025-01-06 17:28:53 +0200

    Delete app directory

--- app/admin_routes.py
+++
@@ -1,8 +0,0 @@
-
-from flask import Blueprint, jsonify
-
-admin = Blueprint("admin", __name__)
-
-@admin.route("/dashboard")
-def dashboard():
-    return jsonify({"message": "Admin Dashboard"}), 200
--- app/client_routes.py
+++
@@ -1,8 +0,0 @@
-
-from flask import Blueprint, jsonify
-
-client = Blueprint("client", __name__)
-
-@client.route("/cart")
-def cart():
-    return jsonify({"message": "User Cart"}), 200
